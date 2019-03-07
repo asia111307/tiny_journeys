f8e82a71b251f350b2ee4427d7186203b4cd235e
asia111307 <aspal13@interia.pl>
2019-03-07 18:28:12 +0100

    moved jumbotron changes to master branch

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,6 @@ const arrow = document.getElementsByClassName('caret-icon-fa')[0];
 const comment_content = document.getElementsByClassName('visible-post-comments-content')[0];
 const comments = document.getElementsByClassName('comment');
 const posts = document.getElementsByClassName('view_post');
-
 function toggleOpenContent() {
     if (arrow.classList.contains('open')) {
         comment_content.style.display = 'none';
@@ -29,8 +28,40 @@ if (arrow) {
 if (comments.length) {
     arrow.click();
 }
-
 for (var i=0; i< posts.length; i+=2) {
     posts[i].style.backgroundColor = 'rgba(0, 0, 0, 0.05)';
     console.log(posts[i]);
 }
+// TITLE ANIMATION -------------------
+const titleAnm = document.querySelector(".title-anm");
+const cursor = document.querySelector(".cursor");
+const title = "Greece my Love";
+const description = document.querySelector(".small-jumb p");
+const descWord = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.";
+let letterNumber = 0;
+const animation = () => {
+    titleAnm.textContent += title[letterNumber++];
+    if (letterNumber === title.length){
+        clearInterval(anmInt);
+        document.querySelector(".underline").style.opacity = "1";
+        letterNumber = 0;
+        setTimeout(()=>{
+            const anmDes = setInterval(()=>{
+                description.textContent += descWord[letterNumber++];
+                if (letterNumber === descWord.length) {
+                    clearInterval(anmDes);
+                    letterNumber = 0;
+                }
+            },80);
+        },1500)
+    }
+};
+const anmInt = setInterval(animation, 250);
+const cursorAnimation = () => {
+    cursor.classList.toggle("cursor-hide");
+};
+setInterval(cursorAnimation, 400);
+document.querySelector(".jumb-video").addEventListener("loadedmetadata", function (){
+        this.currentTime = 27;
+    }, false
+);
