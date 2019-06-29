fbc1e81f552cbbecc890df3bd5d9dddcd629f5dd
asia111307 <aspal13@interia.pl>
2019-06-29 21:24:58 +0200

    autoprefixer added

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var minify = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
+
+function onError(err) {
+    console.log(err);
+}
+
+gulp.task('sass', function(){
+    return gulp.src('static/css/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(minify())
+        .pipe(gulp.dest('static/css/'))
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+});
+
+
